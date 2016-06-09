50f08bd24068842aa718993ac315ec6de3e3d9c5
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-09 11:50:51 -0700

    use remote state az_names

--- main.tf
+++ main.tf
@@ -18,21 +18,17 @@ module "vpc" {
 
   vpc_name = "${var.vpc_name}"
   vpc_cidr = "${var.vpc_cidr}"
-
-  az_count = "${var.az_count}"
-  az_names = "${var.az_names}"
-
-  nat_cidrs = "${var.nat_cidrs}"
 }
 
 module "per_az" {
   source = "../module-aws-per_az"
 
   provider_region = "${var.provider_region}"
+
   vpc_id = "${module.vpc.vpc_id}"
 
   az_count = "${var.az_count}"
-  az_names = "${var.az_names}"
+  az_names = "${terraform_remote_state.global.output.az_names}"
 
   nat_cidrs = "${var.nat_cidrs}"
 }
--- variables.tf
+++ variables.tf
@@ -1,12 +1,11 @@
 variable "provider_region" {}
+variable "bucket_remote_state" {}
+variable "context_org" {}
+variable "context_env" {}
+
+variable "az_count" {}
 
 variable "vpc_cidr" {}
 variable "vpc_name" {}
 
-variable "az_count" {}
-variable "az_names" {}
-
 variable "nat_cidrs" {}
-
-variable "context_org" {}
-variable "bucket_remote_state" {}
