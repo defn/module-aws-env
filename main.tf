8abde9e0e510e60acf1e6e62557a2044ee59a63e
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-14 14:24:53 -0700

    removed provider_region var and use env aws defaults

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  region = "${var.provider_region}"
 }
 
 resource "terraform_remote_state" "global" {
@@ -7,14 +6,11 @@ resource "terraform_remote_state" "global" {
   config {
     bucket = "${var.bucket_remote_state}"
     key = "${var.bucket_remote_state}/env-${var.context_org}-global.tfstate"
-    region = "${var.provider_region}"
   }
 }
 
 module "vpc" {
   source = "../module-aws-vpc"
-
-  provider_region = "${var.provider_region}"
 
   vpc_name = "${var.vpc_name}"
   vpc_cidr = "${var.vpc_cidr}"
