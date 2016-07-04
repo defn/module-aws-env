074db24fdb6653c02b8d9aa4ab27b65a612cd1db
Tom Bombadil <amanibhavam@destructuring.org>
2016-07-04 13:45:34 -0700

    got route53 domains and zones to work per global/env

--- main.tf
+++ main.tf
@@ -3,13 +3,22 @@ provider "aws" { }
 module "vpc" {
   source = "../module-aws-vpc"
 
+  bucket_remote_state = "${var.bucket_remote_state}"
+  context_org = "${var.context_org}"
+  context_env = "${var.context_env}"
+
   vpc_name = "${var.vpc_name}"
   vpc_cidr = "${var.vpc_cidr}"
+  vpc_zone = "${var.vpc_zone}"
 }
 
 resource "aws_sqs_queue" "asg_queue" {
   name = "${var.vpc_name}-asg-queue"
   policy = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Sid\":\"${var.vpc_name}-asg-enqueue\",\"Effect\":\"Allow\",\"Principal\":{\"AWS\":\"arn:aws:iam::${data.terraform_remote_state.global.aws_account_id}:root\"},\"Action\":\"SQS:SendMessage\",\"Resource\":\"arn:aws:sqs:${data.terraform_remote_state.global.aws_region}:${data.terraform_remote_state.global.aws_account_id}:${var.vpc_name}-asg-queue\"}]}"
+}
+
+output "vpc_zone" {
+  value = "${module.vpc.vpc_zone}"
 }
 
 output "vpc_id" {
